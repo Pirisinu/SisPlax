4336d6c667c984abffda61decec6cd408883a453
Pirisinu <palazzialessandro2@gmail.com>
2024-03-19 12:35:15 +0100

    [ add ] fake users data & services data in config

---
+++ config/_my_services.php
@@ -0,0 +1,23 @@
+<?php
+return[
+    [
+        'name' => 'Containers',
+        'price' => 5,
+        'duration_in_month' => 1
+    ],
+    [
+        'name' => 'Virtual Machines',
+        'price' => 10,
+        'duration_in_month' => 1
+    ],
+    [
+        'name' => 'Storage',
+        'price' => 10,
+        'duration_in_month' => 1
+    ],
+    [
+        'name' => 'All Services',
+        'price' =>  180,
+        'duration_in_month' => 12
+    ]
+];
---
+++ config/_my_users.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    [
+      'name' => 'Marco',
+      'surname' => 'Rossi',
+      'email' => 'marcorossi@example.com',
+      'password' => '12345678',
+      'date_of_birth' => '1995/01/01',
+      'isAdmin' => false,
+    ],
+
+    [
+      'name' => 'Jeff',
+      'surname' => 'Bezos',
+      'email' => 'jeffbezos@example.com',
+      'password' => '12345678',
+      'date_of_birth' => '1995/05/02',
+      'isAdmin' => false,
+    ],
+
+
+];
