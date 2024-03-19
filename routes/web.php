8271f208c70bc1f80f2e8fc1312aa47bfdb88c0b
Pirisinu <palazzialessandro2@gmail.com>
2024-03-19 12:33:14 +0100

    [ edit ] Web Routes for user/admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\ServiceController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +17,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/* ROTTE PER UTENTI NON REGISTRATI */
+Route::get('/', [Controller::class, 'index'])->name('home');
+Route::get('/serviceContainer', [Controller::class, 'serviceContainer'])->name('serviceContainer');
+Route::get('/serviceStorage', [Controller::class, 'serviceStorage'])->name('serviceStorage');
+Route::get('/serviceVirtualMachine', [Controller::class, 'serviceVirtualMachine'])->name('serviceVirtualMachine');
 
+
+
+/* ROTTE GESTIONE PROFILO */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/* ROTTE ADMIN PRIVATE */
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('dashboard-services', [ServiceController::class, 'index'])->name('dashboard-services');
+        Route::resource('service', ServiceController::class);
+    });
 
 require __DIR__.'/auth.php';
