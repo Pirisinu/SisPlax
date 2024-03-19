4572605eb375b8ddb3382039cd90ccd0d1859413
Pirisinu <palazzialessandro2@gmail.com>
2024-03-19 17:38:07 +0100

    [ edit ] link in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -32,7 +32,7 @@
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item " href="{{ url('profile') }}">{{__('Profilo')}}</a>
                         @isAdmin
-                            <a class="dropdown-item " href="{{ route('admin.service.index') }}">Dashboard</a>
+                            <a class="dropdown-item " href="{{ route('admin.dashboard-services') }}">Dashboard</a>
                         @endisAdmin
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                               document.getElementById('logout-form').submit();">
