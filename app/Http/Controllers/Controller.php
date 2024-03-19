b7cdce027c9f72887f075cc8c2b2a1994c4d4151
Pirisinu <palazzialessandro2@gmail.com>
2024-03-19 13:42:28 +0100

    [ add ] default route for user not regitered

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index(){
+        return view(('home'));
+    }
+    public function serviceContainer(){
+        return view(('my_services.container'));
+    }
+    public function serviceStorage(){
+        return view(('my_services.storage'));
+    }
+    public function serviceVirtualMachine(){
+        return view(('my_services.virtualMachine'));
+    }
 }
